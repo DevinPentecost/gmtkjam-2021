c8e9d5ef28a8b48c96934f7fa8e74adcb846ffa8
djpentec <devin.j.pentecost@intel.com>
2021-06-11 15:20:48 -0700

    Walker can walk

---
+++ project/objects/player/Anchor.gd
@@ -0,0 +1,17 @@
+extends Area2D
+class_name Anchor
+signal bumped
+
+export(Color) var highlight_color = Color.orange
+export(Color) var dull_color = Color.gray
+
+export(bool) var highlighted = false setget set_highlighted
+
+func set_highlighted(_highlighted:bool):
+	highlighted = _highlighted
+	_refresh_color()
+
+func _refresh_color():
+	var target_color = dull_color
+	if highlighted: target_color = highlight_color
+	modulate = target_color
---
+++ project/objects/player/Walker.gd
@@ -0,0 +1,81 @@
+extends Node2D
+
+enum RotateDirection {
+	CLOCKWISE=1,
+	COUNTER_CLOCKWISE=-1
+}
+
+var current_anchor: Anchor
+var other_anchor: Anchor
+var rotate_direction = RotateDirection.CLOCKWISE
+var rotate_speed = PI / 2  # Rotate PI radians every X second(s)
+
+onready var left_anchor = $Anchor_Left
+onready var right_anchor = $Anchor_Right
+onready var anchor_distance = right_anchor.global_position.distance_to(left_anchor.global_position)
+onready var anchor_angle: float
+
+func _physics_process(delta):
+	_process_rotation(delta)
+
+func _process_rotation(delta):
+	
+	if current_anchor == null or other_anchor == null:
+		return
+	
+	#Amount and direction to rotate
+	anchor_angle += rotate_direction * rotate_speed * delta
+	var anchor_position = current_anchor.global_position
+	var offset = Vector2.DOWN.rotated(anchor_angle) * anchor_distance
+	other_anchor.global_position = anchor_position + offset
+	
+
+func switch_anchor(player_hit_left: bool):
+	#If no current anchor, go the direction hit
+	if current_anchor == null:
+		if player_hit_left:
+			current_anchor = right_anchor
+		else:
+			current_anchor = left_anchor
+	
+	#Move to the next anchor
+	current_anchor.highlighted = false
+	if current_anchor == right_anchor:
+		current_anchor = left_anchor
+		other_anchor = right_anchor
+	else:
+		current_anchor = right_anchor
+		other_anchor = left_anchor
+	current_anchor.highlighted = true
+	
+	anchor_angle = other_anchor.global_position.angle_to_point(current_anchor.global_position) - PI/2
+	
+	#Which way to rotate
+	if player_hit_left:
+		rotate_direction = RotateDirection.COUNTER_CLOCKWISE
+	else:
+		rotate_direction = RotateDirection.CLOCKWISE
+
+func _handle_player_left():
+	switch_anchor(true)
+	
+func _handle_player_right():
+	switch_anchor(false)
+
+	
+func _unhandled_key_input(event):
+	if event.is_action_pressed("player_left"):
+		_handle_player_left()
+	elif event.is_action_pressed("player_right"):
+		_handle_player_right()
+
+func _handle_bump():
+	if rotate_direction == RotateDirection.CLOCKWISE:
+		rotate_direction = RotateDirection.COUNTER_CLOCKWISE
+	else:
+		rotate_direction = RotateDirection.CLOCKWISE
+
+func _on_Anchor_Right_bumped():
+	_handle_bump()
+func _on_Anchor_Left_bumped():
+	_handle_bump()
